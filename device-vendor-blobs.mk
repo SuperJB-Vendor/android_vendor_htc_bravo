a2dfa23b7f6d918e32c0b86ba1bb8edcf9147f9d
podxboq <podxboq@gmail.com>
2012-10-09 20:15:52 +0200

    First JB release.

--- device-vendor-blobs.mk
+++ device-vendor-blobs.mk
@@ -16,8 +16,10 @@
 
 # Prebuilt libraries that are needed to build open-source libraries
 PRODUCT_COPY_FILES := \
-    vendor/htc/bravo/proprietary/libcamera.so:obj/lib/libcamera.so \
-    vendor/htc/bravo/proprietary/libwebkitaccel.so:obj/lib/libwebkitaccel.so
+    vendor/htc/bravo/proprietary/libcamera.so:obj/lib/libcamera.so
+#    vendor/htc/bravo/proprietary/libwebkitaccel.so:obj/lib/libwebkitaccel.so
+
+    
 
 # All the blobs necessary for bravo
 PRODUCT_COPY_FILES += \
@@ -28,17 +30,34 @@ PRODUCT_COPY_FILES += \
     vendor/htc/bravo/proprietary/default.acdb:system/etc/firmware/default.acdb \
     vendor/htc/bravo/proprietary/default_france.acdb:system/etc/firmware/default_france.acdb \
     vendor/htc/bravo/proprietary/default_nel.acdb:system/etc/firmware/default_nel.acdb \
-    vendor/htc/bravo/proprietary/yamato_pfp.fw:system/etc/firmware/yamato_pfp.fw \
-    vendor/htc/bravo/proprietary/yamato_pm4.fw:system/etc/firmware/yamato_pm4.fw \
+    vendor/htc/bravo/proprietary/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
+    vendor/htc/bravo/proprietary/libhtc_ril.so:system/lib/libhtc_ril.so \
+    vendor/htc/bravo/proprietary/libcamera.so:system/lib/libcamera.so \
+    vendor/htc/bravo/proprietary/liboemcamera.so:system/lib/liboemcamera.so
+#    vendor/htc/bravo/proprietary/libOmxVdec.so:system/lib/libOmxVdec.so
+
+# Adreno
+PRODUCT_COPY_FILES += \
+    vendor/htc/bravo/proprietary/eglsubAndroid.so:system/lib/egl/eglsubAndroid.so \
     vendor/htc/bravo/proprietary/libEGL_adreno200.so:system/lib/egl/libEGL_adreno200.so \
     vendor/htc/bravo/proprietary/libGLESv1_CM_adreno200.so:system/lib/egl/libGLESv1_CM_adreno200.so \
     vendor/htc/bravo/proprietary/libGLESv2_adreno200.so:system/lib/egl/libGLESv2_adreno200.so \
-    vendor/htc/bravo/proprietary/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
-    vendor/htc/bravo/proprietary/gps.bravo.so:system/lib/hw/gps.bravo.so \
-    vendor/htc/bravo/proprietary/libcamera.so:system/lib/libcamera.so \
     vendor/htc/bravo/proprietary/libgsl.so:system/lib/libgsl.so \
-    vendor/htc/bravo/proprietary/libhtc_acoustic.so:system/lib/libhtc_acoustic.so \
-    vendor/htc/bravo/proprietary/libhtc_ril.so:system/lib/libhtc_ril.so \
-    vendor/htc/bravo/proprietary/liboemcamera.so:system/lib/liboemcamera.so \
-    vendor/htc/bravo/proprietary/libOmxVdec.so:system/lib/libOmxVdec.so \
-    vendor/htc/bravo/proprietary/libwebkitaccel.so:/system/lib/libwebkitaccel.so
+    vendor/htc/bravo/proprietary/libq3dtools_adreno200.so:system/lib/egl/libq3dtools_adreno200.so \
+    vendor/htc/bravo/proprietary/libsc-a2xx.so:system/lib/libsc-a2xx.so \
+    vendor/htc/bravo/proprietary/yamato_pfp.fw:/system/etc/firmware/yamato_pfp.fw \
+    vendor/htc/bravo/proprietary/yamato_pm4.fw:/system/etc/firmware/yamato_pm4.fw \
+
+# Omx
+PRODUCT_COPY_FILES += \
+    vendor/htc/bravo/proprietary/libOmxVidEnc.so:system/lib/libOmxVidEnc.so
+
+# Vendor library
+PRODUCT_COPY_FILES += \
+    vendor/htc/bravo/proprietary/libqc-opt.so:system/lib/libqc-opt.so
+
+# Media
+PRODUCT_COPY_FILES += \
+    vendor/htc/bravo/proprietary/libWVStreamControlAPI_L1.so:system/vendor/lib/libWVStreamControlAPI_L1.so \
+    vendor/htc/bravo/proprietary/libwvdrm_L1.so:system/vendor/lib/libwvdrm_L1.so \
+    vendor/htc/bravo/proprietary/libwvm.so:system/vendor/lib/libwvm.so
